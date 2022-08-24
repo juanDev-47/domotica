6523ffbefd1cef681d4c20747e2f5ee68bb03a55
juan pablo arenas velez <40310384+juanDev-47@users.noreply.github.com>
2022-08-24 15:47:59 -0500

    Add files via upload

---
+++ MQTT.hpp
@@ -0,0 +1,70 @@
+const char* MQTT_BROKER_ADRESS = "192.168.25.40";
+const uint16_t MQTT_PORT = 1883;
+const char* MQTT_CLIENT_NAME = "ESPClient_1";
+
+// Topics
+const char* DEVICE_TYPE = "Light";
+const char* DEVICE_ID = "0";
+#define RELAY 14
+
+extern int light_state;
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+void SuscribeMqtt()
+{
+  char topic_sub[100];
+  sprintf(topic_sub,"%s/%s", DEVICE_TYPE, DEVICE_ID);
+  mqttClient.subscribe(topic_sub);
+}
+
+String payload;
+void PublisMqtt(unsigned long data)
+{
+   payload = "";
+   payload = String(data);
+   mqttClient.publish("hello/world", (char*)payload.c_str());
+}
+
+void PublisMqttString(char* topic, char* msg)
+{
+   mqttClient.publish(topic, msg);
+}
+
+String content = "";
+void OnMqttReceived(char* topic, byte* payload, unsigned int length) 
+{
+   Serial.print("Received on ");
+   Serial.print(topic);
+   Serial.print(": ");
+
+   content = "";   
+   for (size_t i = 0; i < length; i++) {
+      content.concat((char)payload[i]);
+   }
+   Serial.print(content);
+   Serial.println();
+
+
+   // This code indicates what to do with the received message
+   if((char)payload[0] == '1') {
+      light_state=1;
+      digitalWrite(RELAY, HIGH);
+   }
+   else if ((char)payload[0] == '0') {
+      light_state=0;
+      digitalWrite(RELAY, LOW);
+   }
+   else if ((char)payload[0] == '2') { 
+      // Toggle light 
+      if (light_state == 1){
+        light_state=0;
+        digitalWrite(RELAY, LOW);
+      }
+      else{
+        light_state=1;
+        digitalWrite(RELAY, HIGH);
+      }
+  }
+}
---
+++ Temperature_MQTT_esp32.ino
@@ -0,0 +1,98 @@
+
+/*  Temperature Sensor and light controller
+ *  (Light is represented using the built-in led)
+ *  IoT Course 
+ *  Universidad de Antioquia
+ */
+
+//Library for Wifi connection (included with board info)
+#include <WiFi.h>
+// Library for MQTT connection (by Nick O'Leary)
+#include <PubSubClient.h>
+//Libraries for the temperature sensor
+#include <OneWire.h> // by Paul Stoffregen
+#include <DallasTemperature.h> // by Miles Burton
+
+
+#include "config.h"  // Set your network  SSID and password in this file
+#include "MQTT.hpp"
+#include "ESP32_Utils.hpp"
+#include "ESP32_Utils_MQTT.hpp"
+
+
+//Select the pin you are connecting your temp sensor, pin 4 in my case.
+const int oneWireBus = 4;
+
+// Pin for builtin led in the esp32 board
+#define RELAY 14
+int light_state = 0; // Initial led's state
+
+//Set the onewire interface
+OneWire oneWire(oneWireBus);
+
+//Set the temp sensor to use the onewire interface
+DallasTemperature sensors (&oneWire);
+
+void setup() {
+  pinMode(RELAY, OUTPUT);    // BUILTIN_LED pin as an output
+  digitalWrite(RELAY, LOW);  // Light initial state is OFF
+
+  // Set the serial monitor baud rate
+  Serial.begin(115200);
+  sensors.begin();
+  ConnectWiFi_STA(false);
+  InitMqtt();
+
+}
+
+//Counter for sending keep alive message each 5 seconds
+int counter_KA = 0;
+
+char msg[50];
+
+void loop() {
+
+  // Read the temperature value
+  Serial.print("Sending a command to sensors");
+  sensors.requestTemperatures();
+
+  // Temperature read in celcius
+  float temperatureC = sensors.getTempCByIndex(0);
+
+  // Write data to serial monitor 
+  Serial.print("Temperature sensor : ");
+  Serial.print(temperatureC);
+  Serial.println("°C");
+
+  HandleMqtt();
+
+  // Send temperature value to the MQTT server
+  // Device ID for temperature : 2
+  snprintf (msg, 50, "2,%.2f", temperatureC);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+  PublisMqttString("home", msg);
+
+
+  //Process for sending Keep Alive messages
+  counter_KA++;
+  if(counter_KA == 10){
+    counter_KA = 0;
+    /* Since this ESP32 implemented two devices Light(id = 0) and
+    *  temperature (id = 2), this node must send two KA messages
+    */
+    // DeviceID=0,ONLINE_STATE
+    snprintf (msg, 50, "0,100" );
+    Serial.print("Sending Light keep alive message to Rpi: ");
+    Serial.println(msg);
+    PublisMqttString("home", msg);
+    // DeviceID,ONLINE_STATE
+    snprintf (msg, 50, "2,100" );
+    Serial.print("Sending Temp keep alive message to Rpi: ");
+    Serial.println(msg);
+    PublisMqttString("home", msg);
+  }
+
+  // Read temperature each 5 seconds
+  delay(500);
+}
